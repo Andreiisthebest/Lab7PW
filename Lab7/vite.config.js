6fddc875d97a35bd3bc88cee63bbb30f140c890e
Andreiisthebest <andreibobeica12@gmail.com>
2026-05-11 09:24:33 +0300

    Update vite.config.js

--- Lab7/vite.config.js
+++ Lab7/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Lab6PW/', // Specific base for GitHub Pages with repo name
+  base: '/Lab7PW/', // Specific base for GitHub Pages with repo name
 })
